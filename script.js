1ae7faa7cc420b236709d20c0bfc8679e37a1c78
AMaraza <allymaraza@outlook.com>
2025-03-21 22:58:37 -0400

    Added new color scheme, and added about section, and animation when opening about section

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const aboutSection = document.querySelector('#about_me');
 const aboutArrowsUp = document.getElementsByClassName('fa-solid fa-angle-up');
 const aboutArrowsDown = document.getElementsByClassName('fa-solid fa-angle-down');
 
 let active = false;
 
-function toggleAboutSection(){
+/*function toggleAboutSection(){
 
     if (!active){
-        aboutSection.style.display = "block";
+        aboutSection.style.display = "flex";
         for (let i = 0; i < aboutArrowsUp.length; i++) {
             aboutArrowsUp[i].style.display = "inline";
             aboutArrowsDown[i].style.display = "none";
@@ -23,4 +22,12 @@ function toggleAboutSection(){
         active = false;
     }
 
-}
+}*/
+
+const aboutSection = document.getElementById("about_me");
+const aboutArrow = document.getElementById("about_arrow");
+
+aboutArrow.addEventListener("click", function () {
+    aboutSection.classList.toggle("open");
+    aboutArrow.classList.toggle("open");
+});
