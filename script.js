8c08dad4e6f2a23fbe182a46d551ef1b5bc7dd62
AMaraza <allymaraza@outlook.com>
2025-03-22 13:23:47 -0400

    Added first project section

--- script.js
+++ script.js
@@ -1,33 +1,12 @@
-const aboutArrowsUp = document.getElementsByClassName('fa-solid fa-angle-up');
-const aboutArrowsDown = document.getElementsByClassName('fa-solid fa-angle-down');
-
-let active = false;
-
-/*function toggleAboutSection(){
-
-    if (!active){
-        aboutSection.style.display = "flex";
-        for (let i = 0; i < aboutArrowsUp.length; i++) {
-            aboutArrowsUp[i].style.display = "inline";
-            aboutArrowsDown[i].style.display = "none";
-        }
-        active = true;
-    }
-    else{
-        aboutSection.style.display = "none";
-        for (let i = 0; i < aboutArrowsUp.length; i++) {
-            aboutArrowsUp[i].style.display = "none";
-            aboutArrowsDown[i].style.display = "inline";
-        }
-        active = false;
-    }
-
-}*/
-
 const aboutSection = document.getElementById("about_me");
 const aboutArrow = document.getElementById("about_arrow");
+const webDevCategory = document.getElementById("web_dev");
 
 aboutArrow.addEventListener("click", function () {
     aboutSection.classList.toggle("open");
     aboutArrow.classList.toggle("open");
+});
+
+webDevCategory.addEventListener("click", function () {
+    webDevCategory.classList.toggle("expand");
 });
