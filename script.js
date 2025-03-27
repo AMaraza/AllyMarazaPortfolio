3169b833d89a661db85d796de394fa158b1a5231
AMaraza <allymaraza@outlook.com>
2025-03-27 14:18:53 -0400

    Revamped Portfolio layout from scratch

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
-const aboutSection = document.getElementById("about_me");
-const aboutArrow = document.getElementById("about_arrow");
+const aboutArrow = document.querySelector('.about-arrow');
+const aboutSection = document.querySelector('.about-me');
+const downArrow = document.querySelector('.fas.fa-angle-down');
+const upArrow = document.querySelector('.fas.fa-angle-up');
 
-//Category Sections
-const webDevCategory = document.getElementById("web_dev");
-const tabletopCategory = document.getElementById("tabletop");
-const cppCategory = document.getElementById("cpp");
-
-const categoryContainer = document.getElementById("category_container");
-    aboutArrow.addEventListener("click", function () {
-    aboutSection.classList.toggle("open");
-    aboutArrow.classList.toggle("open");
-});
-
-webDevCategory.addEventListener("click", function () {
-    webDevCategory.classList.toggle("expand");
-    tabletopCategory.classList.toggle("expand_inactive");
-    cppCategory.classList.toggle("expand_inactive");
-    //categoryContainer.classList.toggle("expand");
-});
-
-tabletopCategory.addEventListener("click", function () {
-    webDevCategory.classList.toggle("expand_inactive");
-    tabletopCategory.classList.toggle("expand");
-    cppCategory.classList.toggle("expand_inactive");
-    categoryContainer.classList.toggle("expand");
-});
-
-cppCategory.addEventListener("click", function () {
-    webDevCategory.classList.toggle("expand_inactive");
-    tabletopCategory.classList.toggle("expand_inactive");
-    cppCategory.classList.toggle("expand");
-    categoryContainer.classList.toggle("expand");
-});
+aboutArrow.addEventListener('click', e => {
+    aboutSection.classList.toggle('active');
+    downArrow.classList.toggle('active');
+    upArrow.classList.toggle('active');
+})
