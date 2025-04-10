10e19b74b912c2a26d56dcf2c51fa93e747d3a52
AMaraza <allymaraza@outlook.com>
2025-04-10 17:23:26 -0400

    Collapsable project section

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ aboutArrow.addEventListener('click', e => {
     downArrow.classList.toggle('active');
     upArrow.classList.toggle('active');
 })
+
+const projectCardContainer = document.querySelector('.project-cards');
+const projectCard = document.querySelectorAll('.collapsed-section');
+
+const collapseDetails = document.querySelector('.collapse');
+const projectDetails = document.querySelector('.project-expanded');
+
+for (const project of projectCard){
+    project.addEventListener('click', e => {
+        projectCardContainer.style.display = 'none';
+        projectDetails.style.display = 'block';
+    })
+}
+
+collapseDetails.addEventListener('click', e => {
+    projectDetails.style.display = 'none';
+    projectCardContainer.style.display = 'flex';
+})
