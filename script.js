4984932ace2595a223a5f2c193f055fe6a827298
AMaraza <allymaraza@outlook.com>
2025-04-16 13:56:12 -0400

    Redo of layout to reduce scrolling

--- script.js
+++ script.js
@@ -26,3 +26,40 @@ collapseDetails.addEventListener('click', e => {
     projectDetails.style.display = 'none';
     projectCardContainer.style.display = 'flex';
 })
+
+
+const parentWeb = document.querySelector('#parent-web');
+const parentTabletop = document.querySelector('#parent-tabletop');
+const parentC = document.querySelector('#parent-c');
+
+const containerWeb = document.querySelector('#web');
+const containerTabletop = document.querySelector('#tabletop');
+const containerC = document.querySelector('#c');
+
+parentWeb.addEventListener('click', e => {
+    containerWeb.style.display = "flex";
+    containerTabletop.style.display = 'none';
+    containerC.style.display = 'none';
+
+    parentWeb.style.backgroundColor = "lightgreen";
+    parentTabletop.style.backgroundColor = "white";
+    parentC.style.backgroundColor = "white";
+});
+parentTabletop.addEventListener('click', e => {
+    containerWeb.style.display = "none";
+    containerTabletop.style.display = 'flex';
+    containerC.style.display = 'none';
+
+    parentWeb.style.backgroundColor = "white";
+    parentTabletop.style.backgroundColor = "lightgreen";
+    parentC.style.backgroundColor = "white";
+});
+parentC.addEventListener('click', e => {
+    containerWeb.style.display = "none";
+    containerTabletop.style.display = 'none';
+    containerC.style.display = 'flex';
+
+    parentWeb.style.backgroundColor = "white";
+    parentTabletop.style.backgroundColor = "white";
+    parentC.style.backgroundColor = "lightgreen";
+});
